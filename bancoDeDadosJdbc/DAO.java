e5e44cb8fd4d31997da284624f561b8ab3db89fe
Rander Ornelas <randerornelas@gmail.com>
2021-11-17 21:42:09 -0300

    adiciona DAO

--- bancoDeDadosJdbc/DAO.java
+++ bancoDeDadosJdbc/DAO.java
@@ -1,2 +1,120 @@
-package exercicios.bancoDeDadosJdbc;public class DAO {
+package exercicios.bancoDeDadosJdbc;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAO {
+
+    Connection con;
+
+    public DAO() {
+        this.con = Conexao.getConexao();
+    }
+
+    public List<Pessoa> mostrarTodos() {
+        List<Pessoa> pessoas = new ArrayList<>();
+        ResultSet resultado;
+        String sql = "SELECT * FROM pessoas";
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+            resultado = stmt.executeQuery();
+
+            ResultSetMetaData metaData = resultado.getMetaData();
+            int colunas = metaData.getColumnCount();
+
+            for(int i = 2; i <= colunas; i++) {
+                System.out.printf("%-8s\t\t", metaData.getColumnName(i).toUpperCase());
+            }
+
+            System.out.println();
+
+            while(resultado.next()) {
+                pessoas.add(new Pessoa(resultado.getString("nome"),
+                        resultado.getInt("idade"),
+                        resultado.getString("email")));
+            }
+
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return pessoas;
+    }
+
+    public List<Pessoa> buscarPorNome(String nome) {
+        List<Pessoa> pessoas = new ArrayList<>();
+        ResultSet resultado;
+        String sql = "SELECT * FROM pessoas WHERE nome LIKE ?";
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setString(1, "%" + nome + "%");
+
+            resultado = stmt.executeQuery();
+
+            ResultSetMetaData metaData = resultado.getMetaData();
+            int colunas = metaData.getColumnCount();
+
+            for(int i = 2; i <= colunas; i++) {
+                System.out.printf("%-8s\t", metaData.getColumnName(i).toUpperCase());
+            }
+
+            System.out.println();
+
+            while(resultado.next()) {
+                pessoas.add(new Pessoa(resultado.getString("nome"),
+                        resultado.getInt("idade"),
+                        resultado.getString("email")));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return pessoas;
+    }
+
+    public void adicionar(Pessoa pessoa) {
+        String sql = "INSERT INTO pessoas (nome, idade, email) VALUES (?, ?, ?)";
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setString(1, pessoa.getNome());
+            stmt.setInt(2, pessoa.getIdade());
+            stmt.setString(3, pessoa.getEmail());
+
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void excluir(String email) {
+        String sql = "DELETE FROM pessoas WHERE email = ?";
+
+        try {
+            PreparedStatement stmt = con.prepareStatement(sql);
+
+            stmt.setString(1, email);
+
+            stmt.execute();
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
 }
--- bancoDeDadosJdbc/Operacoes.java
+++ bancoDeDadosJdbc/Operacoes.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 
 public class Operacoes {
 
-    Queries consultas = new Queries();
+    DAO dao = new DAO();
 
     public void mostrarTodos() {
 
         System.out.println();
 
-        for(Pessoa p: consultas.mostrarTodos()) {
+        for(Pessoa p: dao.mostrarTodos()) {
             System.out.printf("%-8s\t\t%-8d\t\t%-8s", p.getNome(), p.getIdade(), p.getEmail());
             System.out.println();
         }
@@ -28,7 +28,7 @@ public class Operacoes {
         System.out.print("Digite um nome para pesquisar: ");
         String nome = entrada.nextLine();
 
-        listaPorNome = consultas.buscarPorNome(nome);
+        listaPorNome = dao.buscarPorNome(nome);
 
         for(Pessoa p: listaPorNome) {
             System.out.printf("%s\t\t%d\t\t%s", p.getNome(), p.getIdade(), p.getEmail());
@@ -38,6 +38,7 @@ public class Operacoes {
 
     public void adicionar() {
 
+        Pessoa pessoa;
         Scanner entrada = new Scanner(System.in);
 
         System.out.println();
@@ -52,38 +53,28 @@ public class Operacoes {
         System.out.println("Digite o e-mail: ");
         String email = entrada.nextLine();
 
-        consultas.adicionarPessoa(nome, idade, email);
+        pessoa = new Pessoa(nome, idade, email);
+
+        dao.adicionar(pessoa);
 
         System.out.println("\nPessoa adicionada com sucesso!");
     }
 
     public void excluir() {
 
-        List<Pessoa> listaPorNome;
-
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println();
-
-        System.out.print("Digite um nome para pesquisar: ");
-        String nome = entrada.nextLine();
-
-        listaPorNome = consultas.buscarPorNome(nome);
-
-        for(Pessoa p: listaPorNome) {
-            System.out.printf("%-8s\t\t%-8d\t\t%-8s", p.getNome(), p.getIdade(), p.getEmail());
-            System.out.println();
-        }
+        buscarPorNome();
 
         System.out.print("\nDigite o e-mail para excluir: ");
         String email = entrada.nextLine();
 
-        consultas.excluirPessoa(email);
+        dao.excluir(email);
 
         System.out.println("\nPessoa excluída com sucesso!");
     }
 
     public void fecharConexao() {
-        consultas.close();
+        dao.close();
     }
 }
